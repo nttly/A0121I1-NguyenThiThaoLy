0688efce245eec2a95f4b218a478e90d3b6fe6ed
Your Name <you@example.com>
2021-04-18 21:20:53 +0700

    nop bai module 2

---
+++ module2/src/introduction_to_java/TH_SuDungToanTu.java
@@ -0,0 +1,15 @@
+package introduction_to_java;
+import java.util.Scanner;
+public class TH_SuDungToanTu {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width:");
+        width=scanner.nextFloat();
+        System.out.println("Enter height:");
+        height=scanner.nextFloat();
+        float area = width*height;
+        System.out.println("Area is:"+area);
+    }
+}
---
+++ module2/src/introduction_to_java/TH_SystemTime.java
@@ -0,0 +1,8 @@
+package introduction_to_java;
+import java.util.Date;
+public class TH_SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is:"+now);
+    }
+}
